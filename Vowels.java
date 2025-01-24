4f41ce27dc283b106de295adb729f67d89d9e64f
vinod-kailodiya <vinodkailodiyaram@gmail.com>
2025-01-24 18:46:29 +0530

    Vowels created

---
+++ Vowels.java
@@ -0,0 +1,24 @@
+public class Vowels{
+    
+public static void main(String[] args){  
+
+   
+  char y ='b'; 
+   
+   if(y == 'a' || y == 'e'|| y == 'i'|| y == 'o' || y == 'u'){
+ 
+ System.out.println("chareter is vowels letter");
+ 
+  }
+
+   else {
+
+ 
+     System.out.println("chareter is not vowels latter");
+}
+
+}
+}
+       
+    
+
