d35234311fa5f2240e5718f224ad64133b9b8a93
vinod-kailodiya <vinodkalodiyaram@ram@gmail.com>
2025-01-24 12:37:16 +0530

    java hitory program added

---
+++ JavaHistory.java
@@ -0,0 +1,11 @@
+public class JavaHistory{
+
+
+ public static void main(String []args){
+
+  System.out.println("Java was conceived by James Gosling at Sun Microsystem in 1991 and marketed in 1995.The initial Name was Oak in 1996 changed to Java and In 2008. Java was acquired by Oracle");
+  
+ 
+
+}
+}
