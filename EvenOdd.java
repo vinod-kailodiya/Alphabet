d193e2094372806212a0182585e11fb9ca4a5b61
vinod-kailodiya <vinodkailodiyaram@gmail.com>
2025-01-24 18:38:43 +0530

    EvenOdd created

---
+++ EvenOdd.java
@@ -0,0 +1,18 @@
+public class EvenOdd{
+
+public static void main(String[] args){
+
+   int a = 6;
+   
+   if(a%2==0 )
+       
+ System.out.println("a is even");
+       
+   else {
+       
+ System.out.println("a is odd");
+}
+
+}
+
+}
